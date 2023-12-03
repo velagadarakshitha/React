586d8ff66b2cc61536a56cad0e6a3bfb3ee7aefd
velagadarakshitha <149819106+velagadarakshitha@users.noreply.github.com>
2023-12-03 14:26:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -75,3 +75,25 @@ const Header = () => {
   );
 };
 //ReactDOM.render(<Header/>,document.getElementById("root"))
+
+//navbar with icon search and logo
+const Header = () => {
+    return (
+      <>
+        <div className='Header'>
+          <div className='logo'>
+          <img src={image} alt="Logo" height="77px" width= "77px" />
+          </div>
+          <div className='text'>
+            <input id='search_type' type='text' placeholder='Search Any Thing'></input>
+            <button id='button'>Search</button>
+          </div>
+          <div className='icon'>
+          <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path fill="#59f221" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
+          </div>
+        </div>
+      </>
+    );
+  };
+  
+  ReactDOM.render(<Header/>, document.getElementById("root"));
